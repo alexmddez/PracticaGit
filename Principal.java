92139787e689b9dcbb4e6ae92cecbcaa459c553c
alexmddez <4362173@alu.murciaeduca.es>
2026-02-05 17:27:32 +0100

    Usuarios

--- Principal.java
+++ Principal.java
@@ -1,14 +1,33 @@
-import java.time.LocalDateTime;
+import java.util.Scanner;
 
-public class  Principal{
-	public static void main(String[]args){
-	
-	System.out.println("Hola Mundo");
-	mostrarFechaHora();
-	}
-	public static void mostrarFechaHora(){
-	LocalDateTime ahora = LocalDateTime.now();
-		System.out.print("Fecha y hora: " + ahora);
+public class Principal {
+  public  static Scanner teclado = new Scanner(System.in);
 
-	}
+    public static void main(String[] args) {
+        Usuario[] usuarios = crearUsuarios(2);
+
+        for (int i = 0; i < usuarios.length; i++) {
+            System.out.println(usuarios[i].nombre + " " + usuarios[i].apellidos + " " + usuarios[i].email);
+        }
+    }
+
+    public static Usuario[] crearUsuarios(int n) {
+        Usuario[] usuarios = new Usuario[n];
+
+        for (int i = 0; i < n; i++) {
+            usuarios[i] = new Usuario();
+
+            System.out.print("Nombre: ");
+            usuarios[i].nombre = teclado.nextLine();
+
+            System.out.print("Apellidos: ");
+            usuarios[i].apellidos = teclado.nextLine();
+
+            System.out.print("Email: ");
+            usuarios[i].email = teclado.nextLine();
+        }
+
+        return usuarios;
+    }
 }
+
