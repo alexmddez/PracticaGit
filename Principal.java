01a756df31efe07ab107935729335334fba333e2
alexmddez <4362173@alu.murciaeduca.es>
2026-02-05 17:03:05 +0100

    Fecha y hora

--- Principal.java
+++ Principal.java
@@ -1,6 +1,14 @@
+import java.time.LocalDateTime;
+
 public class  Principal{
 	public static void main(String[]args){
+	
+	System.out.println("Hola Mundo");
+	mostrarFechaHora();
+	}
+	public static void mostrarFechaHora(){
+	LocalDateTime ahora = LocalDateTime.now();
+		System.out.print("Fecha y hora: " + ahora);
 
-		System.out.println("Hola Mundo");
 	}
 }
